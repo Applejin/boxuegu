a728f392c5c9dfd32b35ef8014c673f37121d78e
xiaohei <2361895375@qq.com>
2017-02-28 10:59:38 +0800

    登录和登出功能，左侧用户头像完成

--- js/common/common.js
+++ js/common/common.js
@@ -3,4 +3,27 @@ define(['jquery'],function($){
         $(this).next().slideToggle();
     });
 
+    // 退出功能
+    $('#logout').on('click',function(){
+        $.post('/v6/logout',function(data){
+            if(data.code == 200){
+                location.href='/html/home/login.html';
+            }
+        });
+
+    });
+
+    // 获取本地cookie用户信息，同时做容错处理
+    var userInfo = null;
+    try {
+        userInfo = JSON.parse($.cookie('userInfo'));
+    }catch(e){
+        userInfo = {};
+    }
+
+    // 然后展示到左侧导航
+    $('.aside .profile h4').html(userInfo.tc_name? userInfo.tc_name : '小黑');
+    $('.aside .profile img').attr('src',userInfo.tc_avatar? userInfo.tc_avatar:'/img/default.png');
+
+
 });
